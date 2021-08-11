61b5b4100e55bfb1d0ddd80cd01b4e16227b4e26
asun.gomez <agcastro7@gmail.com>
2021-08-11 19:56:41 +0200

    Create deployment bucket

--- back_end/deployment/libs/aws.py
+++ back_end/deployment/libs/aws.py
@@ -1,10 +1,21 @@
 import boto3
 import os
 
-def create_deployment_bucket(app_name):
-  session = boto3.Session(profile_name=os.environ['AWS_PROFILE'], region_name=os.environ['REGION'])
-  s3_client = session.client('s3')
 
-  s3_client.create_bucket(
-    Bucket=f"{app_name}-deployment-bucket"
-  )
+def create_deployment_bucket(app_name):
+    session = boto3.Session(
+        profile_name=os.environ['AWS_PROFILE'], region_name=os.environ['REGION'])
+    s3_client = session.client('s3')
+
+    print("Creating S3 bucket")
+    try:
+        s3_client.create_bucket(
+            Bucket=f"{app_name}-deployment-bucket",
+            CreateBucketConfiguration={
+                'LocationConstraint': os.environ['REGION']
+            }
+        )
+    except s3_client.exceptions.BucketAlreadyExists as e:
+        raise e
+    except s3_client.exceptions.BucketAlreadyOwnedByYou:
+        print("Skipping bucket creation")
