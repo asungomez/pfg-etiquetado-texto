591597fce314301bd2ebc2a4b6d20ca1921ea445
asun.gomez <agcastro7@gmail.com>
2021-08-19 19:18:40 +0200

    Add custom message template

--- back_end/lambda/custom_message/libs/templates.py
+++ back_end/lambda/custom_message/libs/templates.py
@@ -1,5 +1,11 @@
+from jinja2 import Environment as JEnvironment
+from jinja2 import FileSystemLoader
+
 def confirm_new_account_template(url):
-  return url
+  env = JEnvironment(loader=FileSystemLoader("/var/task/templates/"))
+  template = env.get_template("confirm_new_account.html")
+  html = template.render(url=url)
+  return html
 
 def forgot_password_template(url):
   return url
