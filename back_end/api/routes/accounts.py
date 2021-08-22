38409866381c7a2f2468a5c3ef67b42e924ada98
asun.gomez <agcastro7@gmail.com>
2021-08-22 18:49:41 +0200

    Error handling

--- back_end/api/routes/accounts.py
+++ back_end/api/routes/accounts.py
@@ -1,4 +1,5 @@
 import os
+import logging
 
 from flask import Blueprint, jsonify, request, redirect
 from resources.utils import account_verification, error_handling
@@ -10,8 +11,14 @@ accounts_endpoint = Blueprint("accounts", __name__)
 @accounts_endpoint.route("/accounts/_validate", methods=["GET"])
 @error_handling
 def validate_account():
-    verification_code = request.args["code"]
-    email = request.args["email"]
-    cognito.confirm_sign_up(email, verification_code)
-    return redirect(f"{os.environ['APP_URL']}/iniciar-sesion/?message=confirmed")
+    email = request.args.get("email")
+    try:
+        verification_code = request.args["code"]
+        cognito.confirm_sign_up(email, verification_code)
+        return redirect(f"{os.environ['APP_URL']}/iniciar-sesion?message=confirmed", code=302)
+    except Exception as e:
+        logging.error(f"Unexpected error: '{e}'")
+        return redirect(f"{os.environ['APP_URL']}/error?message=confirmacion_fallida&email="
+                        f"{email}", code=302)
+
 
